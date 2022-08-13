314814aadcf40ad0c760bfc74818112d39a57089
VladiskavSalitsyn <salitsyn95@mail.ru>
2022-08-13 19:27:47 +0400

    commit 9

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int a = 5;
-        int b = 4;
-        boolean r = a > b;
+        int число = new Scanner(System.in).nextInt();
 
-        String rest = (r == true) ? "a" : "b";
-        System.out.printf(rest);
+        boolean истина = 0 >= число && число <= 12;
+        boolean лож = число >= 12 && число <= 24;
+
+        if (истина) {
+            System.out.printf("Косплее врет");
+        } else {
+            System.out.printf("Косплее говорит правду");
+        }
     }
 }
 /* предварительно создал Git репозиторий на Удаленном сервере GitHub
